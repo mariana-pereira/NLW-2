a87c64760712429d66f8c94c672e5baaab87c979
Mariana Pereira <marianna.22sp@gmail.com>
2020-08-07 19:14:17 -0300

    Quinto dia finalizado

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,13 +1,50 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, ScrollView, AsyncStorage } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import PageHeader from '../../components/PageHeader';
+
 import styles from './styles';
 
 const Favorites: React.FC = () => {
+  const [favorites, setFavorites] = useState([]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      loadFavorites();
+    }, [])
+  )
+  
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if(response) {
+        const favoritedTeachers = JSON.parse(response);
+
+        setFavorites(favoritedTeachers);
+      }
+    });
+  }
+  
   return (
     <View style={styles.container}>
       <PageHeader title="Meus proffys favoritos" />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 24
+        }}
+      >
+        {favorites.map((teacher: Teacher) => (
+          <TeacherItem 
+            key={teacher.id}
+            teacher={teacher}
+            favorited={true}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
 
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
@@ -11,7 +12,17 @@ import heartIcon from '../../assets/images/icons/heart.png';
 import styles from './styles';
 
 const Landing: React.FC = () => {
+  const [totalConnections, setTotalConnections] = useState(0);
+
   const { navigate } = useNavigation();
+
+  useEffect(() => {
+    api.get('connections',).then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    })
+  }, []);
 
   function handleNavigateToGiveClassesPage() {
     navigate('GiveClasses');
@@ -49,7 +60,7 @@ const Landing: React.FC = () => {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 285 conexões já realizadas {' '}
+        Total de {totalConnections} conexões já realizadas {' '}
         <Image source={heartIcon} />
       </Text>
     </View>
